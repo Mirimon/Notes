b78685e31f5c3398551c95e9f11cd45eb840d97b
Alexander Blinov <alexanderblinov@iMac-Alexander.local>
2018-07-27 23:46:18 +0300

    Savint note only if it was really changed or by the Save button press

--- SecurityNotes/Views/NoteDetail.xaml.cs
+++ SecurityNotes/Views/NoteDetail.xaml.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using SecurityNotes.Core.Model;
 using SecurityNotes.Data;
 using Xamarin.Forms;
 
 namespace SecurityNotes {
     public partial class NoteDetail : ContentPage {
+        bool noteWasEdited = false;
+
         public NoteDetail() {
             InitializeComponent();
             MainNavigationPage.Instance.Popped += PagePopped;
         }
 
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            base.OnPropertyChanged(propertyName);
+
+            if(propertyName == "BindingContext") {
+                NoteModel noteModel = BindingContext as NoteModel;
+
+                if (noteModel != null) {
+                    noteModel.PropertyChanged += NoteModel_PropertyChanged;
+                }
+            }
+        }
+
         void PagePopped(object sender, NavigationEventArgs e) {
             if(e.Page == this) {
                 MainNavigationPage.Instance.Popped -= PagePopped;
-                SaveNote();
+                if(noteWasEdited)
+                    SaveNote();
+
+                NoteModel noteModel = BindingContext as NoteModel;
+
+                if (noteModel != null) {
+                    noteModel.PropertyChanged -= NoteModel_PropertyChanged;
+                }
             }
         }
 
@@ -30,6 +52,10 @@ namespace SecurityNotes {
                 noteModel.ChangedTime = DateTime.Now;
                 DataProvider.Instance.SaveNote(noteModel);
             }
+        }
+
+        void NoteModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            noteWasEdited = true;
         }
     }
 }
